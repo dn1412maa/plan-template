08ee32c8924248222690831c731871c04a48c498
dn1412maa <alexander.mustafin@gmail.com>
2017-04-20 16:42:52 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -1,7 +1,7 @@
 plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
   project(key:'HCLC',name:'HipChat Lambda CI')
 
-    stage(name:'Default Stage') {
+    stage(name:'Default Stage', manual: "false") {
       job(key:'JOB1',name:'Build',description:'Build artifacts for AWSLego') {
         task(type:'checkout',description:'Checkout Default Repository',cleanCheckout:'true') {
           repository(name:'tags-repo')
@@ -14,35 +14,4 @@ pip install boto3
 ''')
       }
     }
-}
-deployment(name:'AWSLego CI deployment',planKey:'HCLC-AWSLEGO') {
-  versioning(version:'${bamboo.buildResultKey}')
-
-  environment(name:'Deploy AWSLego to STG') {
-    task(type:'addRequirement',description:'Linux Agent') {
-      requirement(key:'os',condition:'equals',value:'Linux')
-    }
-    task(type:'cleanWorkingDirectory')
-    task(type:'script',description:'Setup venv and create or update stack',
-         scriptBody:'''\
-# Create virtual environment and activate it
-virtualenv venv
-. venv/bin/activate
-
-# Install required packages
-pip install --upgrade pip
-pip install awscli
-
-set -x
-# Stack setup
-echo "Checking if stack lego exists"
-''')
-  }
-
-  environment(name:'Deploy AWSLego to PROD') {
-    task(type:'addRequirement',description:'Linux Agent') {
-      requirement(key:'os',condition:'equals',value:'Linux')
-    }
-    task(type:'cleanWorkingDirectory')
-  }
 }
