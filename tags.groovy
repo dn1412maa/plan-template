04266c1359a1476585ef666285172bd342df8209
dn1412maa <alexander.mustafin@gmail.com>
2017-04-20 16:47:41 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -1,17 +1,55 @@
-plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
+plan(key:'AWSTAGS2',name:'LambdaCI AWS Tags checking and modifying2') {
   project(key:'HCLC',name:'HipChat Lambda CI')
-
-    stage(name:'Default Stage', manual: "false") {
-      job(key:'JOB1',name:'Build',description:'Build artifacts for AWSLego') {
-        task(type:'checkout',description:'Checkout Default Repository',cleanCheckout:'true') {
-          repository(name:'tags-repo')
+    repository(name:'test')
+    repository(name:'test2')
+     
+    stage(name:"stage1", description: "stage1", manual: "false"){
+        job(name: "job1", key: 'job11Key', description: 'hellojob' ){           
+            // checkout --------------------------------------------------------
+            task(type:'checkout', description:'checkout 1'){
+                repository(name:'test')
+            }
+             
+            task(type:'checkout', description:'checkout 2', cleanCheckout: 'true'){
+                repository(name:'test', checkoutDirectory:'./test1directory')
+                repository(name:'test2', checkoutDirectory:'./test2directory')
+            }
+             
+            // script  --------------------------------------------------------
+            task(type:'script', description:'task1', scriptBody:'''
+                    echo hello1
+                    echo hello2
+                '''
+            )
+                         
+            task(type:'script', description:'task2', script:'test1.sh', argument: 'testArg', environmentVariables: 'one', workingSubDirectory: '/one')
+             
+            // Maven 2 and 3 --------------------------------------------------------
+            task(type:'maven3', description:'task3 Simple Maven', goal:'clean install')
+            task(type:'maven3', description:'task3Maven', goal:'clean install', hasTests:'true', useMavenReturnCode:'true')
+            task(type:'maven3', description:'task3Maven more Args', goal:'clean install', hasTests:'true', useMavenReturnCode:'true',
+                environmentVariables: 'one', workingSubDirectory: '/one', testDirectory: '**/target/my_custom/*.xml')
+            task(type:'maven3', description:'task3MavenWithJdk', goal:'clean install', buildJdk:'jdk1.7', mavenExecutable:'maven 3')
+            task(type:'maven2', description:'task2Maven', goal:'clean install', hasTests:'true', useMavenReturnCode:'true')
+                       
+            // junit parser --------------------------------------------------------
+            task(type:'jUnitParser', description:'parse test')
+            task(type:'jUnitParser', description:'parse cucumber results', final:'true', resultsDirectory:'**/TEST-*.xml')
+             
+            // deploy plugin --------------------------------------------------------
+            task(type:'deployBambooPlugin', description:'deploy plugin', url:'http://mybamboo:1990/bamboo',
+                username:'${bamboo.username}', passwordVariable:'${bamboo.password}', artifact:'artifactName')
+             
+            task(type:'deployBambooPlugin', description:'deploy plugin2', url:'http://mybamboo:1990/bamboo',
+                username:'${bamboo.username}', passwordVariable:'${bamboo.password}', artifact:'artifactName',
+                runOnBbranch: 'true')           
+             
+            // make a task final with -> final: 'true' --------------------------------------------------------
+            task(type:'deployBambooPlugin', description:'deploy plugin', url:'http://mybamboo:1990/bamboo',
+                username:'${bamboo.username}', passwordVariable:'${bamboo.password}', artifact:'artifactName', final: 'true')
+             
+            task(type:'jUnitParser', description:'parse cucumber results', resultsDirectory:'**/TEST-*.xml', final:'true')
+             
         }
-        task(type: 'script', description: 'Deployment AWS Stack ',
-                    scriptBody : '''
-virtualenv venv
-source venv/bin/activate
-pip install boto3
-''')
-      }
     }
 }
