12ffd1d93a7a3abc5e4bef110def9c87bd022d85
dn1412maa <alexander.mustafin@gmail.com>
2017-04-24 15:30:52 +0300

    Create awstags-shortcuts.groovy

---
+++ awstags-shortcuts.groovy
@@ -0,0 +1,36 @@
+awstagsRunTests(){
+  task(type:'script',description:'Setup awstags and Run tests',
+     scriptBody:'''
+# Change current directory
+cd lambda/LambdaTagChecker/tests
+
+# Create virtual environment and activate it
+virtualenv venv
+. venv/bin/activate
+
+# Install required packages
+pip install nose
+python setup.py install
+
+# AWS_DEFAULT_REGION variable required for tests
+export AWS_DEFAULT_REGION=us-east-1
+
+# Run awstags tests
+cd tests
+python ../setup.py nosetests --with-xunit
+''')
+}
+
+awstagsUpdateLambdaVariables(['environment']){
+  task(type:'script',description:'Update variables in lambda functions',
+     scriptBody:'''
+# Update variables for lambda function
+
+cd lambda/LambdaTagChecker/
+      sed -i -e "s/<HC_ROOM_ID>/${bamboo_hc_awstags_#environment_hc_room_id}/g" config.yml
+      sed -i -e "s/<HC_AUTH_TOKEN>/${bamboo_hc_awstags_#environment_hc_auth_token}/g" config.yml
+      sed -i -e "s/<BUCKET_NAME>/${bamboo_hc_awstags_#environment_bucket_name}/g" config.yml
+      cat config.yml
+
+''')
+}
