7d8b660a6fdeb936c84ca0a1a1b82e3451bb5503
dn1412maa <alexander.mustafin@gmail.com>
2017-04-24 15:27:19 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -1,11 +1,11 @@
+include(path:'awstags-shortcuts.groovy')
+
 plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
   project(key:'HCLC',name:'HipChat Lambda CI')
   
   stage(name:'Default Stage') {
     job(key:'JOB1',name:'Build',description:'Build artifacts for AWSTags') {
-      artifactDefinition(name:'updater53.py',location:'scripts/lambda/awslego/awslego',pattern:'updater53.py',shared:'true')
-      artifactDefinition(name:'deploy-helper.sh',location:'scripts/lambda/awslego',pattern:'deploy-helper.sh',shared:'true')
-      task(type:'jUnitParser',description:'Parse test results',resultsDirectory:'**/nosetests.xml')
+     task(type:'jUnitParser',description:'Parse test results',resultsDirectory:'**/nosetests.xml')
     }
   }
 }
@@ -24,15 +24,18 @@ deployment(name:'AWSTags CI deployment',planKey:'HCLC-AWSTAGS') {
       # Install required packages
       #pip install --upgrade pip
       #pip install awscli
-      
-      set -x
-      export AWS_ACCESS_KEY_ID=${bamboo_hc_awslego_stg_aws_access_key}
-      export AWS_SECRET_ACCESS_KEY=${bamboo_hc_awslego_stg_aws_password}
-      export AWS_DEFAULT_REGION=${bamboo_hc_awslego_stg_aws_region}
       cd lambda/LambdaTagChecker/
-      sed -i -e "s/<HC_ROOM_ID>/${bamboo_hc_awslego_stg_aws_region}/g" config.yml
+      sed -i -e "s/<HC_ROOM_ID>/${bamboo_hc_awstags_stg_hc_room_id}/g" config.yml
+      sed -i -e "s/<HC_AUTH_TOKEN>/${bamboo_hc_awstags_stg_hc_auth_token}/g" config.yml
+      sed -i -e "s/<BUCKET_NAME>/${bamboo_hc_awstags_stg_bucket_name}/g" config.yml
       cat config.yml
-      zip -r lambda.zip *
+      zip -r lambda_tagchecker.zip *
+
+      set -x
+      export AWS_ACCESS_KEY_ID=${bamboo_hc_awstags_stg_aws_access_key}
+      export AWS_SECRET_ACCESS_KEY=${bamboo_hc_awstags_stg_aws_password}
+      export AWS_DEFAULT_REGION=${bamboo_hc_awstags_stg_aws_region}
+      aws lambda update-function-code --function-name ${bamboo_hc_awstags_stg_lambda_name_checker} --zip-file fileb://lambda.zip
      ''')
   }
 
