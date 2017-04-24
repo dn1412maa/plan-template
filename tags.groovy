8c3e57140c1698a1212caf1d1046a60b9756dfc9
dn1412maa <alexander.mustafin@gmail.com>
2017-04-24 12:09:29 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -3,9 +3,7 @@ plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
 
     stage(name:'Default Stage', manual: "false") {
       job(key:'JOB1',name:'Build',description:'Build artifacts for AWSLego') {
-        task(type:'checkout',description:'Checkout Default Repository',cleanCheckout:'true') {
-          repository(name:'git_dn1412maa/plan-template')
-        }
+
         task(type: 'script', description: 'Deployment AWS Stack ',
                     scriptBody : '''
 virtualenv venv
