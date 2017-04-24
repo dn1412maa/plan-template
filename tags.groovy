bf278683305e6809f7ca0dde86fead2c69e42c27
dn1412maa <alexander.mustafin@gmail.com>
2017-04-24 11:39:39 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -1,25 +1,17 @@
-plan(key:'AWSTAGS2',name:'LambdaCI AWS Tags checking and modifying2') {
+plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
   project(key:'HCLC',name:'HipChat Lambda CI')
-    repository(name:'tags-repo')
-     
-    stage(name:"stage1", description: "stage1", manual: "true"){
-        job(name: "job1", key: 'job11Key', description: 'hellojob' ){           
-            // checkout --------------------------------------------------------
-            task(type:'checkout', description:'checkout 1'){
-                repository(name:'tags-repo')
-            }
-             
-            task(type:'checkout', description:'checkout 2', cleanCheckout: 'true'){
-                repository(name:'tags-repo', checkoutDirectory:'./test1directory')
-            }
-             
-            // script  --------------------------------------------------------
-            task(type:'script', description:'task1', scriptBody:'''
-                    echo hello1
-                    echo hello2
-                '''
-            )
-                        
+
+    stage(name:'Default Stage', manual: "false") {
+      job(key:'JOB1',name:'Build',description:'Build artifacts for AWSLego') {
+        task(type:'checkout',description:'Checkout Default Repository',cleanCheckout:'true') {
+          repository(name:'tags-repo')
         }
+        task(type: 'script', description: 'Deployment AWS Stack ',
+                    scriptBody : '''
+virtualenv venv
+source venv/bin/activate
+pip install boto3
+''')
+      }
     }
 }
