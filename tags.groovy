e33d33402af6f9a476e60876f003f5b774c633e0
dn1412maa <alexander.mustafin@gmail.com>
2017-04-24 12:21:38 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -1,15 +1,26 @@
 plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
   project(key:'HCLC',name:'HipChat Lambda CI')
-
-    stage(name:'Default Stage', manual: "false") {
-      job(key:'JOB1',name:'Build',description:'Build artifacts for AWSLego') {
-
-        task(type: 'script', description: 'Deployment AWS Stack ',
-                    scriptBody : '''
-virtualenv venv
-source venv/bin/activate
-pip install boto3
-''')
-      }
+  
+  stage(name:'Default Stage') {
+    job(key:'JOB1',name:'Build',description:'Build artifacts for AWSLego') {
+      artifactDefinition(name:'updater53.py',location:'scripts/lambda/awslego/awslego',pattern:'updater53.py',shared:'true')
+      artifactDefinition(name:'deploy-helper.sh',location:'scripts/lambda/awslego',pattern:'deploy-helper.sh',shared:'true')
+      task(type:'jUnitParser',description:'Parse test results',resultsDirectory:'**/nosetests.xml')
     }
+  }
 }
+
+deployment(name:'AWSLego CI deployment',planKey:'HCLC-AWSLEGO') {
+
+  environment(name:'Deploy AWSLego to STG') {
+    task(type:'addRequirement',description:'Linux Agent') {
+      requirement(key:'os',condition:'equals',value:'Linux')
+    }
+  }
+
+  environment(name:'Deploy AWSLego to PROD') {
+    task(type:'addRequirement',description:'Linux Agent') {
+      requirement(key:'os',condition:'equals',value:'Linux')
+    }
+  }
+}    
