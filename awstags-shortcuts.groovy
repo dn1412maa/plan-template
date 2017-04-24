01a897fed5dd4f47870eed4a029967054a604c4e
dn1412maa <alexander.mustafin@gmail.com>
2017-04-24 16:11:35 +0300

    Update awstags-shortcuts.groovy

--- awstags-shortcuts.groovy
+++ awstags-shortcuts.groovy
@@ -24,6 +24,7 @@ awstagsUpdateLambdaVariables(['environment']){
   task(type:'script',description:'Update variables in lambda functions',
      scriptBody:'''
 # Update variables for lambda function
+      cd LambdaTagModification
       sed -i -e "s/<HC_ROOM_ID>/${bamboo_hc_awstags_#environment_hc_room_id}/g" config.yml
       sed -i -e "s/<HC_AUTH_TOKEN>/${bamboo_hc_awstags_#environment_hc_auth_token}/g" config.yml
       sed -i -e "s/<BUCKET_NAME>/${bamboo_hc_awstags_#environment_bucket_name}/g" config.yml
@@ -34,7 +35,8 @@ awstagsUpdateLambdaVariables(['environment']){
 
 awstagsArtifactDownload(){
   task(type:'artifactDownload',description:'Download release contents',planKey:'HCLC-AWSTAGS') {
-    artifact(name:'LambdaTagChecker',localPath:'.')
+    artifact(name:'LambdaTagChecker',localPath:'./LambdaTagChecker')
+    artifact(name:'LambdaTagModification',localPath:'./LambdaTagModification')
   }
 }
 
