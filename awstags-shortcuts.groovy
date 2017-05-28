f757106ea17732861df37f137c5c3194b7c01122
dn1412maa <alexander.mustafin@gmail.com>
2017-05-28 15:00:07 +0300

    Update awstags-shortcuts.groovy

--- awstags-shortcuts.groovy
+++ awstags-shortcuts.groovy
@@ -6,10 +6,10 @@ awstagsRunTests(){
 
 # Create virtual environment and activate it
 virtualenv venv
-. venv/bin/activate
+#. venv/bin/activate
 
 # Install required packages
-pip install nose
+#pip install nose
 #python setup.py install
 
 # AWS_DEFAULT_REGION variable required for tests
@@ -24,10 +24,10 @@ awstagsUpdateLambdaVariables(['environment']){
   task(type:'script',description:'Update variables in lambda functions',
      scriptBody:'''
 # Update variables for lambda function
-      cd LambdaTagChecker
-      sed -i -e "s/<HC_ROOM_ID>/${bamboo_hc_awstags_#environment_hc_room_id}/g" config.yml
-      sed -i -e "s/<HC_AUTH_TOKEN>/${bamboo_hc_awstags_#environment_hc_auth_token}/g" config.yml
-      sed -i -e "s/<BUCKET_NAME>/${bamboo_hc_awstags_#environment_bucket_name}/g" config.yml
+#      cd LambdaTagChecker
+#      sed -i -e "s/<HC_ROOM_ID>/${bamboo_hc_awstags_#environment_hc_room_id}/g" config.yml
+#      sed -i -e "s/<HC_AUTH_TOKEN>/${bamboo_hc_awstags_#environment_hc_auth_token}/g" config.yml
+#      sed -i -e "s/<BUCKET_NAME>/${bamboo_hc_awstags_#environment_bucket_name}/g" config.yml
       cat config.yml
 
 ''')
@@ -44,12 +44,12 @@ awstagsArtifactDownload(){
 awstagsUpdateLambdaFunctions(['environment', 'aws_access_key_name', 'aws_secret_key_name']) {
   task(type:'script',description:'Update functions',
      scriptBody:'''
-virtualenv venv     
-. venv/bin/activate
+#virtualenv venv     
+#. venv/bin/activate
 
 # Install required packages
-pip install --upgrade pip
-pip install awscli
+#pip install --upgrade pip
+#pip install awscli
 
 set -x
 export AWS_ACCESS_KEY_ID=${bamboo.hc.awslego.#aws_access_key_name}
@@ -57,9 +57,9 @@ export AWS_SECRET_ACCESS_KEY=${bamboo.hc.awslego.#aws_secret_key_name}
 export AWS_DEFAULT_REGION=${bamboo.hc.awslego.#environment.aws.region}
 
 echo $AWS_ACCESS_KEY_ID
-zip -jr lambda_tagmodification.zip LambdaTagModification
-cd LambdaTagChecker
-zip -r ../lambda_tagchecker.zip *
+#zip -jr lambda_tagmodification.zip LambdaTagModification
+#cd LambdaTagChecker
+#zip -r ../lambda_tagchecker.zip *
 
 #aws lambda update-function-code --function-name ${bamboo_hc_awslego_#environment_lambda_name_checker} --zip-file fileb://../lambda_tagchecker.zip
 
