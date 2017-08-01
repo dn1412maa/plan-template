9fc6396e4700914c3eccb7a1c22e4cc13799f9cf
dn1412maa <alexander.mustafin@gmail.com>
2017-08-01 14:16:16 +0300

    Update tags.groovy

--- tags.groovy
+++ tags.groovy
@@ -12,18 +12,23 @@ plan(key:'AWSTAGS',name:'LambdaCI AWS Tags checking and modifying') {
       repository(name:'tags-repo')
     }      
    
-    task(type:'awsS3',description:'Upload  to s3',pluginVersionOnSave:'2.10.5',
-                resourceAction:'Upload',pluginConfigVersionOnSave:'6',
-                targetBucketName:'ops',artifactToUpload:'LOCAL_FILES',
-                metadataConfigurationJson:'''\
-{
-    "x-amz-acl": "public-read"
-}\
-''',
-                secretKey:'${bamboo.secret_key.password}',sourceLocalPath:'web-client-*.zip',
-                resourceRegion:'us-east-1',accessKey:'${bamboo.access_key}',
-                awsCredentialsSource:'INLINE',awsConnectorId:'-1',
-                targetObjectKey:'h/webpackages/internal/')
+         task(type:'awsS3',description:'Upload web-client-native.zip to s3',
+          scriptBody:'''\
+
+#Env vars for the aws cli
+role_arn=${bamboo.hipchat.aws.stg.role}
+session_uuid=$(/usr/bin/uuidgen)
+creds=$(aws sts assume-role \
+        --role-arn "$role_arn" \
+        --role-session-name "$session_uuid"
+    )
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ACCESS_KEY_ID="$(echo $creds | jq -r .Credentials.AccessKeyId)"
+export AWS_SECRET_ACCESS_KEY="$(echo $creds | jq -r .Credentials.SecretAccessKey)"
+export AWS_SESSION_TOKEN="$(echo $creds | jq -r .Credentials.SessionToken)"
+
+aws s3 cp web-client-native.zip s3://hipchat-ops/hipchat4/webpackages/nightly/web-client-native.zip --acl public-read
+''')
      
     }
   }
